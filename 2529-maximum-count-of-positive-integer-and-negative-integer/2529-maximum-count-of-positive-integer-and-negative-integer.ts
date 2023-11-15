1fa1149cf2144d37cc03ed3452348021c5f69510
dan <46671380+mugyeol@users.noreply.github.com>
2023-11-15 16:54:21 +0900

    Time: 61 ms (40.00%), Space: 45.4 MB (25.00%) - LeetHub

---
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.ts
@@ -0,0 +1,31 @@
+  function maximumCount(nums: number[]): number {
+    let negLeft = 0
+    let negRight = nums.length-1
+    let negCount = 0
+    while(negLeft <= negRight){
+        const mid = Math.floor((negLeft+negRight)/2)
+        if(nums[mid]===0){
+            negRight = negRight-1
+        }else if(nums[mid]<0){
+            negCount +=1
+            negLeft = negLeft +1
+        }else if(nums[mid]>0){
+            negRight = negRight -1
+        }
+    }
+    let posLeft = 0
+    let posRight = nums.length-1
+    let posCount = 0
+    while(posLeft <= posRight){
+        const mid = Math.floor((posLeft+posRight)/2)
+        if(nums[mid]===0){
+            posLeft = posLeft+1
+        }else if(nums[mid]<0){
+            posLeft = posLeft +1
+        }else if(nums[mid]>0){
+            posCount+=1
+            posRight = posRight -1
+        }
+    }
+    return Math.max(posCount, negCount)
+};
