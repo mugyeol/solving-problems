b5aec4054e75666f43cbd49f859f3166d9d4c6c5
dan <46671380+mugyeol@users.noreply.github.com>
2023-11-17 20:08:56 +0900

    Time: 66 ms (43.39%), Space: 45.7 MB (13.49%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.ts
+++ 0101-symmetric-tree/0101-symmetric-tree.ts
@@ -12,35 +12,35 @@
  * }
  */
 
-// function isSymmetric(root: TreeNode | null): boolean {
-//     let queue = [root.left, root.right]
+function isSymmetric(root: TreeNode | null): boolean {
+    let queue = [root.left, root.right]
     
-//     while(queue.length>0){
-//         let node1 = queue.shift()
-//         let node2 = queue.shift()
+    while(queue.length>0){
+        let node1 = queue.shift()
+        let node2 = queue.shift()
         
-//         if(node1 ===null && node2===null )continue;
-//         if(node1===null || node2 ===null) return false;
-//         if(node1.val !== node2.val) return false;
+        if(node1 ===null && node2===null )continue;
+        if(node1===null || node2 ===null) return false;
+        if(node1.val !== node2.val) return false;
         
-//         queue.push(node1.left)
-//         queue.push(node2.right)
-//         queue.push(node1.right)
-//         queue.push(node2.left)
+        queue.push(node1.left)
+        queue.push(node2.right)
+        queue.push(node1.right)
+        queue.push(node2.left)
     
-//     }
-//     return true
+    }
+    return true
     
-// };
-
-const isSymmetric = (root: TreeNode | null): boolean => {
-  return isMirror(root, root);
 };
+
+// const isSymmetric = (root: TreeNode | null): boolean => {
+//   return isMirror(root, root);
+// };
   
-const isMirror = (node1: TreeNode | null, node2: TreeNode | null): boolean => {
-  if (node1 === null && node2 === null) return true;
-  if (node1 === null || node2 === null) return false;
-  if(node1.val !== node2.val) return false;
+// const isMirror = (node1: TreeNode | null, node2: TreeNode | null): boolean => {
+//   if (node1 === null && node2 === null) return true;
+//   if (node1 === null || node2 === null) return false;
+//   if(node1.val !== node2.val) return false;
   
-  return isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left);
-}
+//   return isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left);
+// }
