22970b453e8ab6d8925d87bd71d10ff40a00a7d9
dan <46671380+mugyeol@users.noreply.github.com>
2024-03-05 06:56:07 +0900

    Time: 54 ms (77.50%), Space: 52.1 MB (6.01%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.ts
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -1,22 +1,63 @@
 /**
  Do not return anything, modify nums1 in-place instead.
  */
-function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    let p1 = m-1, p2 = n-1, i = m+n -1
+function merge(nums1, m, nums2, n) {
+    // nums1 = [0], m=1, nums2 = [1], n=1
     
-    while(p1>=0 && p2>=0){
-        if(nums1[p1] > nums2[p2]){
-            nums1[i] = nums1[p1]
-            p1--
+    let firstPointer = m-1, secondPointer = n-1, reverseIndex = m+n-1
+    
+    while (firstPointer >= 0 && secondPointer >=0){
+        if(nums1[firstPointer] > nums2[secondPointer]){
+            nums1[reverseIndex] = nums1[firstPointer]
+            firstPointer--;
         }else{
-            nums1[i] = nums2[p2]
-            p2--
+            nums1[reverseIndex] = nums2[secondPointer]
+            secondPointer--;
         }
-        i--
+        reverseIndex--;
     }
-        while(p2>=0){
-            nums1[i] = nums2[p2]
-            p2--
-            i--
-        }
-};
+    
+    while(secondPointer >=0){
+        nums1[reverseIndex] = nums2[secondPointer]
+        console.log(secondPointer)
+        secondPointer--;
+        reverseIndex--;
+    }
+    
+    
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     let p1 = m - 1, p2 = n - 1, i = m + n - 1;
+
+//     while (p1 >= 0 && p2 >= 0) {
+//         if (nums1[p1] > nums2[p2]) {
+//             nums1[i] = nums1[p1];
+//             p1--;
+//         } else {
+//             nums1[i] = nums2[p2];
+//             p2--;
+//         }
+//         i--;
+//     }
+
+//     while (p2 >= 0) {
+//         nums1[i] = nums2[p2];
+//         p2--;
+//         i--;
+//     }
