e90c899a8e3b322a75733122e370f10d22d7f881
dan <46671380+mugyeol@users.noreply.github.com>
2024-03-10 18:22:16 +0900

    Time: 56 ms (68.52%), Space: 51.7 MB (20.71%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.ts
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -2,28 +2,27 @@
  Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1, m, nums2, n) {
-    // nums1 = [0], m=1, nums2 = [1], n=1
-    
-    let firstPointer = m-1, secondPointer = n-1, reverseIndex = m+n-1
-    
-    while (firstPointer >= 0 && secondPointer >=0){
-        if(nums1[firstPointer] > nums2[secondPointer]){
-            nums1[reverseIndex] = nums1[firstPointer]
-            firstPointer--;
-        }else{
-            nums1[reverseIndex] = nums2[secondPointer]
-            secondPointer--;
-        }
-        reverseIndex--;
-    }
-    
-    while(secondPointer >=0){
-        nums1[reverseIndex] = nums2[secondPointer]
-        secondPointer--;
-        reverseIndex--;
-    }
-    
-    
-    
 
+ let p1 = m -1, p2 = n-1, index = m + n -1
+ 
+ while (p1 >=0 && p2>=0){
+     if(nums1[p1] > nums2[p2]){
+         nums1[index] = nums1[p1]
+         p1--
+     }else { 
+        nums1[index] = nums2[p2]
+         p2--
+     
+     }
+     index--
+ }
+    
+    while(p2>=0) {
+        nums1[index] = nums2[p2]
+        index --
+        p2--
+    }
+    
+    
+    
 }
