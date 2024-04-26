814ede53f96665f8472cca0ae41956c2890a2c58
dan <46671380+mugyeol@users.noreply.github.com>
2024-04-27 04:35:17 +0900

    Time: 67 ms (39.30%), Space: 53.8 MB (51.08%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.ts
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.ts
@@ -13,8 +13,11 @@
  */
 
 function maxDepth(root: TreeNode | null): number {
-    if(root === null){
+ 
+    if(root===null){
         return 0
     }
-    return (1+Math.max(maxDepth(root.left), maxDepth(root.right)))
+    return (1+ Math.max(maxDepth(root.left), maxDepth(root.right)))
+    
+    
 };
