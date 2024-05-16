6e2bf86335563ce2b58afdfd52f22c41fc4fe856
dan <46671380+mugyeol@users.noreply.github.com>
2024-05-17 01:51:34 +0900

    Time: 59 ms (88.12%), Space: 53.7 MB (95.69%) - LeetHub

--- 0112-path-sum/0112-path-sum.ts
+++ 0112-path-sum/0112-path-sum.ts
@@ -11,12 +11,15 @@
  *     }
  * }
  */
+
 function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
-    if(root === null){
-            return false
-    }
-    if(root.left===null && root.right === null){
-        return root.val === targetSum
-    }
-    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val)
+
+if(root===null) return false
+    
+if(root.left === null && root.right===null){
+    return root.val === targetSum 
 }
+    const newTargetSum = targetSum - root.val
+    return hasPathSum(root.left, newTargetSum ) || hasPathSum(root.right, newTargetSum)
+    
+};
